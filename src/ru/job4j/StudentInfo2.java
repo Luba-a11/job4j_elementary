895d9dac4a1b52de42b1f03dfab4e7103735ee8d
LubaEremenko <eremenko.luba79@gmail.com>
2021-06-22 12:03:58 +0100

    student info

---
+++ src/ru/job4j/StudentInfo2.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo2 {
+    public static void main(String[] args) {
+        System.out.println("Eremenko Liubov");
+        System.out.println("00.00.00");
+    }
+}
