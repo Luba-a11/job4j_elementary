417734b7e362eb28e18c31f2d2f0b7732b7546f5
LubaEremenko <eremenko.luba79@gmail.com>
2021-07-04 12:32:48 +0100

    4.1. Аргументы [#295521 #170321] fixed

---
+++ src/ru/job4j/blacNote.java
@@ -0,0 +1,19 @@
+package ru.job4j;
+
+import java.util.Scanner;
+
+public class blacNote {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter first number");
+        int num1 = input.nextInt();
+        System.out.println("enter second number");
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.printf("Sum of your numbers %d%n", sum);
+        System.out.println("sum is " + sum);
+
+    }
+}
--- src/ru/job4j/calculator2/ArgMethod2.java
+++ src/ru/job4j/calculator2/ArgMethod2.java
@@ -3,15 +3,15 @@ package ru.job4j.calculator2;
 import java.util.Stack;
 
 public class ArgMethod2 {
-    public static void hello(String name) {
-        System.out.println("Hello, " + name);
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", age is " + age);
 
     }
 
     public static void main(String[] args) {
         String name =  "Petr Arsentev";
         int age = 33;
-        ArgMethod2.hello(name);
+        ArgMethod2.hello(name, age);
     }
 
 }
