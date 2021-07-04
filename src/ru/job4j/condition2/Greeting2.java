49c77456c82da676e5d8c9d4f550006914dfeb5f
LubaEremenko <eremenko.luba79@gmail.com>
2021-07-04 20:15:33 +0100

    9.1. String [#237407]

---
+++ src/ru/job4j/condition2/Greeting2.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition2;
+
+public class Greeting2 {
+    public static void main(String[] args) {
+        String idea =  "I like Java!";
+        System.out.println(idea);
+
+        String dop =  " But I am a newbie. ";
+        System.out.println(idea + dop);
+
+        int year = 2021;
+        String id = idea + dop +year;
+        System.out.println(id);
+
+    }
+}
