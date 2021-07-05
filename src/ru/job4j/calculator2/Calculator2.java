cbc12ee1e5fa019f9a3f8c6f53d1d2f616a28162
LubaEremenko <eremenko.luba79@gmail.com>
2021-07-05 18:57:38 +0100

    1.1. Что такое import [#504773]

--- src/ru/job4j/calculator2/Calculator2.java
+++ src/ru/job4j/calculator2/Calculator2.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculator2;
+import static ru.job4j.calculator2.MathFunc2.*;
 
 public class Calculator2 {
+    public static int sum2(int func3, int func4) {
+        return func3 + func4;
+    }
+
+    public static int sum4(int func1, int func2, int func3, int func4) {
+        return func1 + func2 + func3 + func4;
+    }
+
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
@@ -14,6 +23,12 @@ public class Calculator2 {
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
+
+        System.out.println(Calculator2.sum2(5, 8));
+        System.out.println(Calculator2.sum4(89, 4, 1, 6));
+
+        System.out.println("Результат суммы 2-х функций равен " + Calculator2.sum2(33, 145));
+        System.out.println("Результат суммы 4-х функций равен " + Calculator2.sum4(33, 145, 2, 5));
 
     }
 }
--- src/ru/job4j/calculator2/MathFunc2.java
+++ src/ru/job4j/calculator2/MathFunc2.java
@@ -2,13 +2,22 @@ package ru.job4j.calculator2;
 
 public class MathFunc2 {
     public static int func1(int x) {
-    int y1 = x * x + 1;
-    return y1;
+        return x * x + 1;
+
     }
 
     public static int func2(int x) {
-        int y2 = x * 10;
-        return y2;
+        return x * 10;
+
+    }
+
+    public static int func3(int x) {
+        return x - 10;
+
+    }
+
+    public static int func4(int x) {
+        return x / 3;
 
     }
 
@@ -16,9 +25,13 @@ public class MathFunc2 {
         int result1 = MathFunc2.func1(3);
         int result2 = MathFunc2.func2(7);
         int result3 = MathFunc2.func1(100);
+        int rslt4 = MathFunc2.func3(155);
+        int rslt5 = MathFunc2.func4(99);
         int total = result1  + result2;
         System.out.println(total);
         System.out.println(result3);
+        System.out.println(rslt4);
+        System.out.println(rslt5);
 
     }
 
