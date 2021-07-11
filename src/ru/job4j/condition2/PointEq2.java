bfc70663a2265b14a3a8ebeb4dca1de2614b02b3
LubaEremenko <eremenko.luba79@gmail.com>
2021-07-11 23:11:52 +0100

    4.3.4. if c return [#333785 #175415] fixed

--- src/ru/job4j/condition2/PointEq2.java
+++ src/ru/job4j/condition2/PointEq2.java
@@ -2,14 +2,13 @@ package ru.job4j.condition2;
 
 public class PointEq2 {
     public static boolean eq(int x1, int y1, int x2, int y2) {
-        boolean eqX = x1 == x2;
-        boolean eqY = y1 == y2;
-        return eqX && eqY;
+        return x1 == x2 && y1 == y2;
+
     }
 
     public static void main(String[] args) {
-        System.out.println(PointEq2.eq(1, 1, 2, 2));
-        System.out.println(PointEq2.eq(5, 5, 5, 5));
+        System.out.println(PointEq2.eq(1, 1, 1, 1));
+        System.out.println(PointEq2.eq(5, 0, 5, 5));
     }
 
 }
