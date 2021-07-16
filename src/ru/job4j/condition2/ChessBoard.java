5da37e65402133f10193f5d769e8cf67ac4f21a1
LubaEremenko <eremenko.luba79@gmail.com>
2021-07-16 21:07:57 +0100

    4.3.6. Math.abs [#373616]

--- src/ru/job4j/condition2/ChessBoard.java
+++ src/ru/job4j/condition2/ChessBoard.java
@@ -3,15 +3,30 @@ package ru.job4j.condition2;
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         int rsl = 0;
-        if(x1 == x2 +7 || y1 == y2 + 7) {
-            rsl = Math.abs((x2 + 7) - x1);
-            rsl = rsl == 0 ? Math.abs((y2 +7) - y1) : rsl;
+        if(x1 == x2 + 1 && y1 == y2 + 1) {
+            rsl = Math.abs((x2 + 1) - x1);
+            rsl = rsl == 0 ? Math.abs((y2 + 1) - y1) : rsl;
         }
+        if (x1 == x2 + 1 || y1 == y2 - 1) {
+            rsl = Math.abs((x2 + 1) - x1);
+            rsl = rsl == 0 ? Math.abs((y2 - 1) - y1) : rsl;
+
+        }
+        if(x1 == x2 - 1 || y1 == y2 - 1) {
+            rsl = Math.abs((x2 - 1) - x1);
+            rsl = rsl == 0 ? Math.abs((y2 - 1) - y1) : rsl;
+        }
+        if (x1 == x2 - 1 || y1 == y2 + 1) {
+            rsl = Math.abs((x2 - 1) - x1);
+            rsl = rsl == 0 ? Math.abs((y2 + 1) - y1) : rsl;
+
+        }
+
         return rsl;
 
     }
 
     public static void main(String[] args) {
-        System.out.println(ChessBoard.way(6, 7, 1, 2));
+        System.out.println(ChessBoard.way(6, 7, 5, 2));
     }
 }
