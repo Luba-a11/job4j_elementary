95f4caa8bd5ad73b9d31c60bfb562e6a89686fa6
LubaEremenko <eremenko.luba79@gmail.com>
2021-07-18 17:34:40 +0100

    4.3.6. Math.abs [#373616] fixed

--- src/ru/job4j/condition2/ChessBoard.java
+++ src/ru/job4j/condition2/ChessBoard.java
@@ -3,23 +3,29 @@ package ru.job4j.condition2;
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         int rsl = 0;
-        if(x1 == x2 + 1 && y1 == y2 + 1) {
+      /*  if(x1 == x2 + 1 && y1 == y2 + 1) {
             rsl = Math.abs((x2 + 1) - x1);
             rsl = rsl == 0 ? Math.abs((y2 + 1) - y1) : rsl;
         }
-        if (x1 == x2 + 1 || y1 == y2 - 1) {
+        if (x1 == x2 + 1 && y1 == y2 - 1) {
             rsl = Math.abs((x2 + 1) - x1);
             rsl = rsl == 0 ? Math.abs((y2 - 1) - y1) : rsl;
 
         }
-        if(x1 == x2 - 1 || y1 == y2 - 1) {
+        if(x1 == x2 - 1 && y1 == y2 - 1) {
             rsl = Math.abs((x2 - 1) - x1);
             rsl = rsl == 0 ? Math.abs((y2 - 1) - y1) : rsl;
         }
-        if (x1 == x2 - 1 || y1 == y2 + 1) {
+        if (x1 == x2 - 1 && y1 == y2 + 1) {
             rsl = Math.abs((x2 - 1) - x1);
             rsl = rsl == 0 ? Math.abs((y2 + 1) - y1) : rsl;
 
+        }*/
+
+        if (x1 >= 0 && x2 <= 7 && y1 >= 0 && y2 <= 7) {
+            if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
+                rsl = Math.abs(x2 - x1);
+            }
         }
 
         return rsl;
