dd58d395dfa88133cab278ab062fbe7f5621a616
LubaEremenko <eremenko.luba79@gmail.com>
2021-07-26 10:14:47 +0100

    5.5. Простое число [#171694 #179711] fixed

---
+++ src/ru/job4j/loop2/PrimeNumber2.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop2;
+
+public class PrimeNumber2 {
+    public static int calc(int finish) {
+        int count = 0;
+        for (int number = 1; number <= finish; number++) {
+            if (CheckPrimeNumber2.check(number)) {
+             count = count + 1;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int count = PrimeNumber2.calc(10);
+        System.out.println(count);
+    }
+}
