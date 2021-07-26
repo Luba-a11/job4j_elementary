86a38913d9e9e9622e5d4688f1d277b7db63f393
LubaEremenko <eremenko.luba79@gmail.com>
2021-07-26 10:14:47 +0100

    5.6. Простые числа [#156314]

--- src/ru/job4j/loop2/CheckPrimeNumber2.java
+++ src/ru/job4j/loop2/CheckPrimeNumber2.java
@@ -2,10 +2,12 @@ package ru.job4j.loop2;
 
 public class CheckPrimeNumber2 {
     public static boolean check(int number) {
-        boolean prime = true;
-        for (int i = 2; i <= number / 2; i++) {
-            if (number % i == 0) {
-                  return false;
+        boolean prime = number > 1;
+        for (int i = 2; i <= number; i++) {
+            int temp = number % i;
+            if (temp == 0) {
+                prime = false;
+                 break;
 
             }
         }
