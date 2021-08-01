28ebacb177a52b1d16b4e92b452a25f21f28b6a7
LubaEremenko <eremenko.luba79@gmail.com>
2021-08-01 07:49:27 +0100

    6.5.2. Поиск минимума в диапазоне. [#179804]

---
+++ test/ru/job4j/array2/MinDiapason2Test.java
@@ -0,0 +1,41 @@
+package ru.job4j.array2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MinDiapason2Test {
+
+    @Test
+    public void whenFirstMin() {
+        int[] array = new int[] {-1, 0, 5, 10};
+        int start = 1;
+        int finish = 3;
+        int rslt = MinDiapason2.findMin(array, start, finish);
+        int expected = 0;
+        Assert.assertEquals(expected, rslt);
+
+    }
+
+    @Test
+    public void whenLastMin() {
+        int[] array = new int[] {10, 5, 3, 1};
+        int start = 1;
+        int finish = 3;
+        int result = MinDiapason2.findMin(array, start, finish);
+        int expected = 1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenMiddleMin() {
+        int[] array = new int[] {10, 2, 5, 1};
+        int start = 0;
+        int finish = 2;
+        int result = MinDiapason2.findMin(array, start, finish);
+        int expected = 2;
+        Assert.assertEquals(expected, result);
+    }
+
+}
