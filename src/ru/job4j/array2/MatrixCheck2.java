9ddac618e8d30432238dcb6f374301ce157f4684
LubaEremenko <eremenko.luba79@gmail.com>
2021-08-02 13:26:43 +0100

    6.7.2. Моно столбец в матрице. [#214127]

--- src/ru/job4j/array2/MatrixCheck2.java
+++ src/ru/job4j/array2/MatrixCheck2.java
@@ -1,5 +1,7 @@
 package ru.job4j.array2;
 
+import javax.print.DocFlavor;
+
 public class MatrixCheck2 {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
@@ -11,5 +13,17 @@ public class MatrixCheck2 {
         }
 
         return result;
+    }
+
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean rslt = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] != 'X') {
+                rslt = false;
+                break;
+            }
+
+        }
+        return rslt;
     }
 }
--- test/ru/job4j/array2/MatrixCheck2Test.java
+++ test/ru/job4j/array2/MatrixCheck2Test.java
@@ -30,4 +30,31 @@ public class MatrixCheck2Test {
         boolean rslt = MatrixCheck2.monoHorizontal(board, row);
         Assert.assertFalse(rslt);
     }
+
+    @Test
+    public void whenHasMonoVertical() {
+        char[][] board = {
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+        };
+        int column = 2;
+        boolean rslt = MatrixCheck2.monoVertical(board, column);
+        Assert.assertTrue(rslt);
+    }
+
+    @Test
+    public void whenHasFalseMonoVertical() {
+        char[][] board = {
+                {' ', ' ', 'X'},
+                {' ', ' ', ' '},
+                {' ', ' ', 'X'},
+        };
+        int column = 2;
+        boolean rslt = MatrixCheck2.monoVertical(board, column);
+        Assert.assertFalse(rslt);
+    }
+
+
+
 }
