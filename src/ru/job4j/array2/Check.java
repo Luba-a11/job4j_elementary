55bb8acbbbebeede1e1f7e2d22e8059bc6ff4d38
LubaEremenko <eremenko.luba79@gmail.com>
2021-08-02 12:13:40 +0100

    6.5.2. Поиск минимума в диапазоне. [#179804 #184235] fixed2

--- src/ru/job4j/array2/Check.java
+++ src/ru/job4j/array2/Check.java
@@ -3,7 +3,7 @@ package ru.job4j.array2;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean rslt = true;
-        for (int i = 0; i < data.length; i++) {
+        for (int i = 1; i < data.length; i++) {
             if (data[i] != data[0]) {
                 rslt = false;
                 break;
